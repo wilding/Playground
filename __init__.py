9c3bcc5652af4cd3bf3339e1b30a391b58854980
wilding <wildingthrelfall@gmail.com>
2016-09-29 01:26:43 -0700

    testing
        - form testing
        - datetime testing
        - timedelta testing
        - ARRAY testing
        - JSON testing

--- __init__.py
+++ __init__.py
@@ -1,8 +1,10 @@
-from flask import Flask, render_template, url_for, jsonify, redirect, request, flash
+from flask import Flask, render_template, url_for, json, redirect, request, flash
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from database_setup import Base
+from database_setup import Base, Test
+
+from datetime import datetime, timedelta
 
 app = Flask(__name__)
 
@@ -14,7 +16,72 @@ session = DBSession()
 
 @app.route('/')
 def homepage():
-    return render_template('homepage.html')
+    # session.rollback()
+    tests = session.query(Test).all()
+    return render_template('homepage.html', tests=tests)
+
+
+@app.route('/test/new/', methods=['GET', 'POST'])
+def newTest():
+    if request.method == 'POST':
+        form = parseForm(request.form)
+        newtest = Test(
+            integer=form['integer'],
+            floatpt=form['floatpt'],
+            string=request.form['string'],
+            uni=request.form['uni'],
+            boolean=form['boolean'],
+            date=form['date'],
+            time=form['time'],
+            datetime=form['datetime'],
+            interval=timedelta(days=form['days'], seconds=form['seconds'], microseconds=form['microseconds']),
+            int_array=form['int_array'],
+            str_array=form['str_array'],
+            json_object=form['json_object']
+            )
+        session.add(newtest)
+        session.commit()
+        flash('New Test Added!')
+        return redirect(url_for('homepage'))
+    else:
+        return render_template('newtest.html')
+
+
+@app.route('/formplayground/')
+def formPlayground():
+    return render_template('formplayground.html')
+
+
+# helper function
+def parseForm(form):
+    form_dict = {}
+    form_dict['integer'] = form['integer'] if form['integer'] else 0
+    form_dict['floatpt'] = form['floatpt'] if form['floatpt'] else 0.0
+    form_dict['boolean'] = True if form.get('boolean') else False
+    form_dict['date'] = datetime.strptime(form['date'], '%Y-%m-%d').date() if form['date'] else None
+    form_dict['time'] = datetime.strptime(form['time'], '%H:%M').time() if form['time'] else None
+    form_dict['datetime'] = datetime.strptime(form['datetime'], '%Y-%m-%dT%H:%M') if form['datetime'] else None
+    form_dict['days'] = int(form['days']) if form['days'] else 0
+    form_dict['seconds'] = int(form['seconds']) if form['seconds'] else 0
+    form_dict['microseconds'] = int(form['microseconds']) if form['microseconds'] else 0
+    int_array1 = int(form['int_array1']) if form['int_array1'] else 0
+    int_array2 = int(form['int_array2']) if form['int_array2'] else 0
+    int_array3 = int(form['int_array3']) if form['int_array3'] else 0
+    form_dict['int_array'] = [int_array1, int_array2, int_array3]
+    form_dict['str_array'] = [form['str_array1'], form['str_array2'], form['str_array3']]
+    json_object = {
+        "instructions": [
+            {
+                "text": 1,
+                "image": 2,
+                "video": 3,
+                "gif": 4,
+                "note": 5
+            }
+        ]
+    }
+    form_dict['json_object'] = json.dumps(json_object)
+    return form_dict
 
 
 if __name__ == '__main__':
--- database_setup.py
+++ database_setup.py
@@ -1,10 +1,38 @@
-from sqlalchemy import Column, ForeignKey, Integer, String, Boolean, Date, DateTime, Enum, Float, Interval, PickleType, Time, Unicode
-# from sqlalchemy import Array, JSON
+from sqlalchemy import Column, ForeignKey, Integer, String, Boolean, Date, DateTime, Float, Interval, Time, Unicode
+# from sqlalchemy import Numeric, BigInteger, SmallInteger, Text, UnicodeText, Enum, PickleType, LargeBinary, MatchType, SchemaType
+from sqlalchemy.dialects.postgresql import ARRAY, JSON
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
 
+# from enum import Enum
+
+
+# class MyEnum(enum.Enum):
+#     one = 'one'
+#     two = 'two'
+#     three = 'three'
+
+
 Base = declarative_base()
+
+
+class Test(Base):
+    __tablename__ = 'test'
+    id = Column(Integer, primary_key=True)
+    integer = Column(Integer)
+    floatpt = Column(Float)
+    string = Column(String(500))
+    uni = Column(Unicode)
+    boolean = Column(Boolean)
+    date = Column(Date)
+    time = Column(Time)
+    datetime = Column(DateTime)
+    interval = Column(Interval)
+    int_array = Column(ARRAY(Integer))
+    str_array = Column(ARRAY(String(9999)))
+    json_object = Column(JSON)
+    # enum = Column(Enum(MyEnum))
 
 
 engine = create_engine('postgresql://vagrant:password@localhost/playground')
